3d0bb8e32aec058e085bf8cfe3e2556d85aef4ab
Gustavo Straub <gustavostraubcovers@gmail.com>
2020-06-03 18:13:25 -0300

    more mutations and also error treatment

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -4,8 +4,8 @@ const Note = require('../models/Note')
 const Resolver = {
   Query: {
     Users: () => User.find(),
-    User: async (_, {id}) => await User.findById(id),
-
+    User: async (_, { id }) => await User.findById(id),
+    Note: async (_, { id }) => await Note.findById(id),
     Notes: () => Note.find(),
 
   },
@@ -14,17 +14,39 @@ const Resolver = {
     addUser: async (_, args) => {
       return await new User(args).save()
     },
+    editUser: (_, args) => {
+      if (args.email) return User.findOneAndUpdate({ email: args.email }, args)
+      if (args.id) return User.findOneAndUpdate({ _id: args.id }, args)
+      if (!args.email || !args.id) return new Error('You need to inform either an ID or an Email in order to edit a user.')
+    },
+    deleteUser: async (_, { id, email }) => {
+      if (id) return await User.findByIdAndDelete({ _id: id })
+      if (email) return await User.findOneAndDelete({ email: email })
+      if (!id || !email) return Error('You need to inform either an ID or an Email in order to delete a user')
+    },
     addNote: async (_, args) => {
-      return await new Note(args).save()
+      if(args) return await new Note(args).save()
+      if(!args.title) return Error('Please, inform a title for your note')
+      if(!args.note) return Error('Please, inform the note for your note')
+      if(!args.userId) return Error('Please inform the user')
+      
+    },
+    editNote: (_, args) => {
+      if (args.id) return Note.findOneAndUpdate({ _id: args.id }, args)
+      return new Error('You need to inform an ID in order to edit a note')
+    },
+    deleteNote: (_, { id }) => {
+      if (id) return Note.findByIdAndDelete({ _id: id })
+      return new Error('You need to inform an ID in order to delete a note')
     }
   },
 
   User: {
-    notes: (parent, args) => Note.find({userId: parent.id})
+    notes: (parent, args) => Note.find({ userId: parent.id })
   },
-  
+
   Notes: {
-    user: (parent, args) => User.findById({_id: parent.userId})
+    user: (parent, args) => User.findById({ _id: parent.userId })
   },
 
 }
--- schema/schema.js
+++ schema/schema.js
@@ -17,14 +17,24 @@ const typeDefs = gql`
   }
 
   type Query {
+    # Users Queries
     Users: [User!]!
-    User(id: ID!): User
+    User(id: ID, email:String): User
+
+    # Notes Queries
     Notes: [Notes!]!
+    Note(id:ID!): Notes!
   },
 
   type Mutation {
+    # User mutations
     addUser(name: String!, email:String!, password: String!):User
+    editUser(id: String, name: String, email:String, password:String):User
+    deleteUser(id: String, email:String):User
+    # Notes Mutations
     addNote(title: String!, note: String!, userId: String!):Notes
+    editNote(id: String,title: String, note:String):Notes
+    deleteNote(id:String!):Notes
   }
  
 `;
